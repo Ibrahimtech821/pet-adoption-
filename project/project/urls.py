3d30c26a8ba871d0d13515d5079ad170dd758a1a
ibrahim <roma.ahmed19042005@gmail.com>
2025-10-30 23:06:45 +0300

    make url in project to empty to open directly to home or login (depend on what we want to do)

--- project/project/urls.py
+++ project/project/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pet_adoption/',include("petadoption.urls")),
+    path('',include("petadoption.urls")),
 ]
