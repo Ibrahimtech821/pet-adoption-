4f015a24f84856bbad7fd76f05c1678976ecb896
Ibrahimtech821 <roma.ahmed19042005@gmail.com>
2025-12-08 20:19:10 +0200

    Add PostgreSQL support and update dependencies
    - Updated Pipfile to include psycopg2-binary and dj-database-url for PostgreSQL support.
    - Modified Pipfile.lock to reflect the new package versions and hashes.
    - Created data.json with initial log entries and permissions for the admin interface.
    - Added users.json with predefined user data for authentication.
    - Updated settings.py to configure PostgreSQL database connection on supapass.

--- project/project/settings.py
+++ project/project/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -73,12 +75,21 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres.ihkkoktodvfezgpuxkix',
+        'PASSWORD': 'youcantseeme',
+        'HOST': 'aws-1-eu-central-1.pooler.supabase.com',
+        'PORT': '6543',
     }
 }
+
+
+
 
 
 # Password validation
