ee90ba8aa3e18e6291c46107cabff762b65ccfc7
nada <elghaweetnada@gmail.com>
2025-12-12 22:36:35 +0200

    did pet quiz and result templates

--- project/petadoption/urls.py
+++ project/petadoption/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib import admin
-from .views import MyLoginView,home,about,register,pet_detail,submit_adoption_request, adoption_success,pet_quiz,my_requests,logout_user, pets_view,search_pets
+from .views import MyLoginView,home,about,register,pet_detail,submit_adoption_request, adoption_success,pet_quiz,my_requests,logout_user, pets_view,search_pets, pet_quiz_result
 
 urlpatterns= [
     path('',home,name="home"),
@@ -13,10 +13,10 @@ urlpatterns= [
     path('pets/<int:pet_id>/', pet_detail, name='pet_detail'),
     path('adopt/<int:pet_id>/', submit_adoption_request, name='submit_adoption_request'),
     path('adoption-success/', adoption_success, name='adoption_success'),
-    path('quiz/', pet_quiz , name='pet_quiz'),
     path('my_requests/',my_requests, name='my_requests'),
     path('logout/',logout_user, name='logout'),
-
+    path('quiz/', pet_quiz , name='pet_quiz'),
+    path('quiz/result/', pet_quiz_result, name='pet_quiz_result'),
     
 
 ]
